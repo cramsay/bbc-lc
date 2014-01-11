385b9f00e4304b4538a3df7d9a48c5355a203f6d
Craig Ramsay <cramsay01@gmail.com>
2014-01-11 16:45:00 +0000

    Shows ALL events. Not just what is shown on webpage

--- getEvents.php
+++ getEvents.php
@@ -8,6 +8,9 @@ if($argc != 2){
 // Create DOM from URL or file
 $html = file_get_html('http://polling.bbc.co.uk/sport/shared/football/oppm/live-text-commentary/' . $argv[1]);
 
+//Remove introductary "Live Text Commentary" h2
+$html->find('#live-text-introduction',0)->innertext = "";
+
 // Add in tags for ncurses formatting
 // Event Highlighting
 foreach($html->find('p[class=event]') as $e){
@@ -16,7 +19,7 @@ foreach($html->find('p[class=event]') as $e){
 
 $fname = $argv[1] . ".html";
 $f = fopen($fname,"w");
-fwrite($f,$html->find('#live-text',0));
+fwrite($f,$html->find('#live-text-commentary-wrapper',0));
 fclose($f);
 print shell_exec('links -dump ' . $fname);
 shell_exec('rm ' . $fname);
